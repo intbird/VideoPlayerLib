a90d4d7d53f6af2e9878713fa5a9a059725f751c
intbird <intbird@163.com>
2020-11-04 15:08:35 +0800

    video sized

--- videoplayer/src/main/java/net/intbird/soft/lib/video/player/main/player/player/ExoPlayerImpl.kt
+++ videoplayer/src/main/java/net/intbird/soft/lib/video/player/main/player/player/ExoPlayerImpl.kt
@@ -3,11 +3,21 @@ package net.intbird.soft.lib.video.player.main.player.player;
 import android.content.Context
 import android.net.Uri
 import android.text.TextUtils
+import com.google.android.exoplayer2.ExoPlayerLibraryInfo
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.PlaybackParameters
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.database.ExoDatabaseProvider
+import com.google.android.exoplayer2.source.DefaultMediaSourceFactory
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
+import com.google.android.exoplayer2.upstream.HttpDataSource
+import com.google.android.exoplayer2.upstream.cache.CacheDataSource
+import com.google.android.exoplayer2.upstream.cache.NoOpCacheEvictor
+import com.google.android.exoplayer2.upstream.cache.SimpleCache
 import com.google.android.exoplayer2.util.EventLogger
 import com.google.android.exoplayer2.util.MimeTypes
 import net.intbird.soft.lib.video.player.api.error.MediaError
@@ -17,6 +27,7 @@ import net.intbird.soft.lib.video.player.main.player.call.IPlayerCallback
 import net.intbird.soft.lib.video.player.main.player.player.event.ExoPlayerStateHandler
 import net.intbird.soft.lib.video.player.main.player.mode.MediaFileInfo
 import net.intbird.soft.lib.video.player.utils.MediaLogUtil
+import java.io.File
 
 /**
  * created by intbird
@@ -31,14 +42,48 @@ class ExoPlayerImpl(
     private val playerListDelegate: PlayerListDelegate?,
     private val playerCallback: IPlayerCallback?
 ) : IPlayer {
-    private var player = SimpleExoPlayer.Builder(context).build()
-    private var playerEvent = ExoPlayerStateHandler(player, playerCallback)
+    private lateinit var player:SimpleExoPlayer
+    private lateinit var playerEvent:ExoPlayerStateHandler
 
     private var mediaFileInfo: MediaFileInfo? = null
 
     init {
+        initSimpleExoPlayer()
         playerView?.player = player
         player?.addAnalyticsListener(EventLogger(DefaultTrackSelector(context)))
+    }
+
+    private fun initSimpleExoPlayer() {
+//        // Build a HttpDataSource.Factory with cross-protocol redirects enabled.
+//        val httpDataSourceFactory: HttpDataSource.Factory = DefaultHttpDataSourceFactory(
+//            ExoPlayerLibraryInfo.DEFAULT_USER_AGENT,
+//            DefaultHttpDataSource.DEFAULT_CONNECT_TIMEOUT_MILLIS,
+//            DefaultHttpDataSource.DEFAULT_READ_TIMEOUT_MILLIS,  /* allowCrossProtocolRedirects= */
+//            true
+//        )
+//
+//        val cacheDataSource = CacheDataSource.Factory()
+//            .setCache(
+//                SimpleCache(
+//                File("exoplayer-temps"),
+//                NoOpCacheEvictor(),
+//                ExoDatabaseProvider(context)
+//            )
+//            )
+//            .setUpstreamDataSourceFactory(httpDataSourceFactory)
+//            .setCacheWriteDataSinkFactory(null)
+//            .setFlags(CacheDataSource.FLAG_IGNORE_CACHE_ON_ERROR)
+//
+//        // Wrap the HttpDataSource.Factory in a DefaultDataSourceFactory, which adds in
+//        // support for requesting data from other sources (e.g., files, resources, etc).
+//        val dataSourceFactory = DefaultDataSourceFactory(context, cacheDataSource)
+//
+//        player = SimpleExoPlayer.Builder(context)
+//            .setMediaSourceFactory(DefaultMediaSourceFactory(dataSourceFactory))
+//            .build()
+
+        player = SimpleExoPlayer.Builder(context).build()
+        playerEvent = ExoPlayerStateHandler(player, playerCallback)
     }
 
     override fun onParamsChange(mediaFileInfo: MediaFileInfo?) {
@@ -62,7 +107,6 @@ class ExoPlayerImpl(
 
         val mediaItem: MediaItem = MediaItem.Builder()
             .setUri(uri)
-            .setDrmLicenseRequestHeaders(headers)
             .setSubtitles(arrayListOf(mediaSubtitle))
             .build()
         player?.setMediaItem(mediaItem)
